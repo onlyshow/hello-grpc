2847bc684d17c07a03fb7632a84e694d8cdc86a4
onlyshow <707433323@qq.com>
2019-03-25 21:57:49 +0800

    ..

--- example/client.php
+++ example/client.php
@@ -3,28 +3,27 @@
 ini_set('display_errors', 'stderr');
 require __DIR__ . "/../vendor/autoload.php";
 
-//$sf = new \SensioLabs\Consul\ServiceFactory([
-//    'base_uri' => 'http://0.0.0.0:18500',
-//]);
-//
-///** @var \SensioLabs\Consul\Services\Health $health */
-//$health = $sf->get('health');
-//
-//$services = $health->service('grpc', [
-//    'passing' => true,
-//])->json();
-//
-//$healthServices = [];
-//foreach ($services as $service) {
-//    $healthServices[] = $service['Service'];
-//}
-//var_dump(array_column($healthServices, 'Address', 'Port'));
-//
-//$key = array_rand($healthServices, 1);
-//$service = $healthServices[$key];
-//
-//$hostname = $service['Address'] . ':' . $service['Port'];
-$hostname = '0.0.0.0:9001';
+$sf = new \SensioLabs\Consul\ServiceFactory([
+    'base_uri' => 'http://0.0.0.0:18500',
+]);
+
+/** @var \SensioLabs\Consul\Services\Health $health */
+$health = $sf->get('health');
+
+$services = $health->service('grpc', [
+    'passing' => true,
+])->json();
+
+$healthServices = [];
+foreach ($services as $service) {
+    $healthServices[] = $service['Service'];
+}
+var_dump(array_column($healthServices, 'Address', 'Port'));
+
+$key = array_rand($healthServices, 1);
+$service = $healthServices[$key];
+
+$hostname = $service['Address'] . ':' . $service['Port'];
 
 $client = new \Service\EchoClient($hostname, [
     'credentials' => \Grpc\ChannelCredentials::createInsecure(),
